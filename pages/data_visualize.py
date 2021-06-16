e407d61bf3aa91c5b6d471a8d627990f547e8398
Prakhar Rathi <prakharrathi25@gmail.com>
2021-06-16 21:16:15 +0530

    German Variables translation

--- pages/data_visualize.py
+++ pages/data_visualize.py
@@ -1,12 +1,19 @@
 # Import necessary libraries 
+from operator import index
 import os
+import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns 
+import seaborn as sns
+from seaborn.external.docscrape import indent 
+from scipy.stats import skew, kurtosis
     
 # from pandas._config.config import options 
 import streamlit as st 
 import base64
+
+# Custom modeules 
+from .utils import *
 
 
 # Create the app that would be run 
@@ -26,14 +33,40 @@ def app():
         ''' Display a correlation matrix '''
         corr_matrix = st.checkbox("Do you want a correlation matrix?", value=False)
         if corr_matrix:
-            pass 
+            sns.heatmap(data.corr(), annot=True)
+            st.pyplot() 
 
-        ''' Select Display Parameters '''
+        ''' Select Display Parameters and plot the charts '''
         col1, col2 = st.beta_columns(2)
 
         # Select the feature 
-        feature = col1.selctbox("Select the feature you want to visualize.", options=list(data.columns), index=4)
+        feature = col1.selectbox("Select the feature you want to visualize.", options=list(data.columns), index=5)
 
         # Select the type of chart you want 
+        chart = col2.selectbox("Select the type of chart you want to see.", 
+                                options=["Histogram", "Distribution", "Line Plot"], index=1)
 
-        
+        # Plot a histogram 
+        if chart == "Distribution":
+            
+            kde_check = st.checkbox("Show Kernel Density Estimation?", value=False)
+            # pLot distribution 
+            if kde_check: 
+                sns.distplot(data[feature], 10, kde=True)
+            else: 
+                sns.distplot(data[feature], 10, kde=False)
+            
+            st.pyplot()
+
+             # Add the stats about skewness and kurtosis 
+            skewness = skew(data[feature])
+            excess_kurtosis = kurtosis(data[feature])
+
+            st.w
+
+
+        elif chart == "Histogram":
+
+            # calculate the histogram edges
+            data_histogram(data[feature], 10, None, None)
+            st.pyplot()
--- pages/utils.py
+++ pages/utils.py
@@ -1,9 +1,11 @@
 ''' Functions that need to be used regularly '''
 
 # Import necessary libraries 
+from numpy.core.fromnumeric import size
 import pandas as pd 
 import streamlit as st 
 import numpy as np 
+import matplotlib.pyplot as plt 
 
 # Function to fix the format of ag5 
 def fix_ags5(x):
@@ -16,14 +18,35 @@ def fix_ags5(x):
 @st.cache() # caching to reduce time 
 def get_english_term(german_phrase):
     
+        
     # This is loading the data each time so there has to be a smarter way to do this. Might help reducing time. 
-    translate = pd.read_csv('data/metadata/col_translate.csv')
+    translate = pd.read_csv('../data/metadata/col_translate.csv')
 
     german_phrase = german_phrase.upper()
-    print(german_phrase)
-    print(translate[translate['German_Variable'] == german_phrase])
+#     print(german_phrase)
+#     print(translate[translate['Variablenname'] == german_phrase])
     
     try: 
-        return translate[translate['German_Variable'] == german_phrase]['Translation'].values[0] 
+        return translate[translate['Variable_name'] == german_phrase]['variable_english'].values[0] 
     except: 
         return german_phrase.lower()
+
+
+''' Visualisation Functions '''
+
+# Funtion to plot a histogram 
+def data_histogram(column, no_of_bins, minimum=None, maximum=None):
+    
+    # default vals
+    if minimum is None:
+        minimum = min(column)
+    if maximum is None:
+        maximum = max(column)
+        
+    # calculate the histogram edges
+    bin_edges = np.arange(minimum, maximum, (maximum - minimum)/no_of_bins)
+    
+    plt.figure(figsize=(5,5))
+    plt.hist(column, bin_edges)
+      
+    plt.show()
