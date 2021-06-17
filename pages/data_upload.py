66b8964cb0cf3179c43ab5d31aa688e02ee282da
CinnyLin <45028889+CinnyLin@users.noreply.github.com>
2021-06-18 01:17:12 +0800

    map annotation with advanced filters!

--- pages/data_upload.py
+++ pages/data_upload.py
@@ -50,27 +50,27 @@ def app():
     data_cols = data.columns
     col_to_display = st.selectbox("Select which column to visualise on the map",
                                  options=data_cols, 
-                                 index=4
+                                 index=6
                                 #  format_func = lambda x: get_english_term(x)
                                  )
 
-    ''' Display the document containing the various column descriptions '''
+    # ''' Display the document containing the various column descriptions '''
     
-    # Check if the data description needs to be displayed 
-    display_doc = st.radio("Display Column Descriptions", options=["Yes", "No"], index=1)
-    if display_doc == "Yes":
+    # # Check if the data description needs to be displayed 
+    # display_doc = st.radio("Display Column Descriptions", options=["Yes", "No"], index=1)
+    # if display_doc == "Yes":
 
-        # Read a pdf of the data dictionary
-        pdf_file_path = 'documents\pdf\coronadata_description.pdf'
-        with open(pdf_file_path,"rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    #     # Read a pdf of the data dictionary
+    #     pdf_file_path = 'documents\pdf\coronadata_description.pdf'
+    #     with open(pdf_file_path,"rb") as f:
+    #         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
         
-        # HTML for the file to be displayed 
-        pdf_display = f'<embed src="data:application/pdf;base64, {base64_pdf}" width="700" height="1000" type="application/pdf">'
-        st.markdown(pdf_display, unsafe_allow_html=True)
+    #     # HTML for the file to be displayed 
+    #     pdf_display = f'<embed src="data:application/pdf;base64, {base64_pdf}" width="700" height="1000" type="application/pdf">'
+    #     st.markdown(pdf_display, unsafe_allow_html=True)
 
     ''' Display the Kreis Map '''
-    st.header(f"The Map visualisation for **{col_to_display}** at the Kreiss level.")
+    st.header(f"The Map visualisation for **{col_to_display}** at the Kreis-level.")
    
     # Fix the ag5 column using function defined in utils
     data['ags5_fix'] = data['ags5'].apply(fix_ags5)
@@ -91,8 +91,27 @@ def app():
     merged = gpd.GeoDataFrame(merged)
 
     # Check if the labels need to be added 
-    labels = st.radio("Show labels?", options=["Yes", "No"], index=1)
+    labels = st.radio("Show all labels?", options=["Yes", "No"], index=1)
 
+    # Check if certain labels need to be added -- by region
+    label_ags = st.radio("Show labels by region?", options=["Yes", "No"], index=1)
+    if label_ags == "Yes": 
+        bundeslands = list(range(1,17))
+        txt_to_display_ags = st.selectbox("Select which Bundesland to annotate",
+                                    options=bundeslands, index=0)
+
+    # Check if certain labels need to be added -- by stats
+    label_stats = st.radio("Show labels by stats?", options=["Yes", "No"], index=1)
+    if label_stats == "Yes": 
+        stats = ['mean', 'min', '25%', '50%', '75%', 'max']
+        stats_values = merged[col_to_display].describe()[['mean', 'min', '25%', '50%', '75%', 'max']].sort_values()
+        st.write(stats_values)
+        # txt_to_display_stats = st.selectbox("Select which range to annotate",
+        #                             options=stats, index=1)
+        txt_to_display_stats = st.slider("Select a range of values", 
+                                        float(stats_values['min']), float(stats_values['max']), 
+                                        (float(stats_values['25%']), float(stats_values['75%'])))
+    
     # plot
     fig, ax = plt.subplots(figsize=(50,30))
     merged.plot(column=col_to_display, scheme="quantiles",
@@ -100,8 +119,29 @@ def app():
                 cmap='coolwarm', legend=True)
 
     ax.set_title(f'{col_to_display} in Germany by County', fontsize=15)
+
+    # filters
+    # (1) by ags
+    if label_ags == "Yes": 
+        # get filtered df
+        merged_ags = merged[merged['ags2']==txt_to_display_ags]
+        # add text with filters
+        for i in merged_ags.index:
+            ax.text(merged_ags.longitude[i], merged_ags.latitude[i],
+                    f'{merged_ags["kreis"][i]}\n{merged_ags[col_to_display][i]}', fontsize=10)
     
-    # add text
+    # (2) by stats
+    if label_stats == "Yes": 
+        # get filtered df
+        merged_stats = merged[
+            (merged[col_to_display]>=txt_to_display_stats[0]) & 
+            (merged[col_to_display]<=txt_to_display_stats[1])]
+        # add text with filters
+        for i in merged_stats.index:
+            ax.text(merged_stats.longitude[i], merged_stats.latitude[i],
+                    f'{merged_stats["kreis"][i]}\n{merged_stats[col_to_display][i]}', fontsize=10)
+    
+    # add all text
     if labels == "Yes": 
         for i in range(len(merged)):
             ax.text(merged.longitude[i], merged.latitude[i],
