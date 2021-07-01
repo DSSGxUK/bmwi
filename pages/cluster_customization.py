ecb5ab5de3e2398492e4a15c2a3a3f06c539b34f
CinnyLin <45028889+CinnyLin@users.noreply.github.com>
2021-07-01 19:02:56 +0800

    cluster customization UI

--- pages/cluster_customization.py
+++ pages/cluster_customization.py
@@ -22,23 +22,32 @@ def app():
     #st.write("Uploaded dataset size:", data.shape)
 
     data['kreis_options'] = data['ags5'].astype(str)+' '+data['kreis']
+    data['cluster'] = data['cluster'].astype(str)
     clusters = sorted(list(data['cluster'].unique()))
+    cluster_dict = {}
 
+    ### bug: one kreis could be in multiple clusters but would only be reflected on the first one assigned
     for i in clusters:
-        st.multiselect(label=f"Which kreis are in cluster {i}?", 
-            options=list(data['kreis_options']),
-            default=list(data[data['cluster']==int(i)]['kreis_options']) )
-            #help="If not is selected, the variable will be treated as numerical.")
-    
-    # export customized clusters
-    # df_cluster = data[['ags5', 'kreis', 'km_cluster']]
-    # st.markdown(get_table_download_link(df_cluster, text="Download Cluster Results"), unsafe_allow_html=True)
+        cluster_list = st.multiselect(label=f"Which kreis are in cluster {i}?", 
+                        options=list(data['kreis_options']),
+                        default=list(data[data['cluster']==i]['kreis_options']) )
+                        #help="If not is selected, the variable will be treated as numerical.")
+        cluster_dict[i]=cluster_list
 
+    def assign_cluster(x):
+        for i in cluster_dict.keys():
+            if x in cluster_dict[i]:
+                return i
+    data['cluster'] = data['kreis_options'].apply(assign_cluster)
+    
     ''' Cluster Visualisation '''
-    ### this needs to be based on customized cluster results
     button_run = st.radio("Cluster visualization", options=["Yes", "No"], index=1)
     if button_run == "Yes": 
         st.subheader("Cluster Map")
         data['ags5_fix'] = data['ags5'].apply(fix_ags5)
         fig = data_map(data, 'ags5_fix', 'cluster', cat_col=True)
         st.pyplot(fig)
+    
+    # export customized clusters
+    df_cluster = data[['ags5', 'kreis', 'cluster']]
+    st.markdown(get_table_download_link(df_cluster, text="Download Cluster Results"), unsafe_allow_html=True)
